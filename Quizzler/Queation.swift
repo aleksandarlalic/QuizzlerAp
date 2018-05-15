f0150c30254dfa4a1d65113d113199bf37d96244
aleksandar lalic <aleksandarlalic87@gmail.com>
2018-05-15 22:12:54 +0200

    Final Commit

---
+++ Quizzler/Queation.swift
@@ -0,0 +1,25 @@
+//
+//  Queation.swift
+//  Quizzler
+//
+//  Created by aleksandar lalic on 08/01/2018.
+//
+//
+
+import Foundation
+
+class Question {
+    
+    let qusetionText : String
+    let answer : Bool
+    
+    init(text: String, correctAnswer: Bool) {
+        qusetionText = text
+        answer = correctAnswer
+    }
+    
+    
+}
+
+    
+
